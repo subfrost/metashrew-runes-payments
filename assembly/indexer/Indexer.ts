e3a19bd840dcd86cc27105f20be06817ffe47e19
ignoblehierarch <ignoblehierarch@proton.me>
2024-08-01 11:51:23 -0500

    update

---
+++ assembly/indexer/Indexer.ts
@@ -0,0 +1,8 @@
+import { Block } from "metashrew-as/assembly/blockdata/block";
+import { RunesIndex } from "metashrew-runes/assembly/indexer";
+
+export class RunesPayment extends RunesIndex {
+    indexBlock(height: u32, _block: Block): void {
+        
+    }
+}
