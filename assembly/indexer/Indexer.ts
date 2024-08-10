f4db51bcc0747135494a5d98916cd716a96c14db
ignoblehierarch <ignoblehierarch@proton.me>
2024-08-09 19:10:19 -0500

    update

--- assembly/indexer/Indexer.ts
+++ assembly/indexer/Indexer.ts
@@ -3,11 +3,12 @@ import { RunesBlock } from "metashrew-runes/assembly/indexer/RunesBlock";
 import { RunestoneMessage } from "metashrew-runes/assembly/indexer/RunestoneMessage";
 import { RunesTransaction } from "metashrew-runes/assembly/indexer/RunesTransaction";
 import { OUTPOINT_TO_RUNES } from "metashrew-runes/assembly/indexer/constants";
+import { BalanceSheet } from "metashrew-runes/assembly/indexer/BalanceSheet";
+import { RUNES_PAYMENTS_TABLE } from "../tables/tables";
 
 export class RunesPayment extends RunestoneMessage {
     indexBlock(height: u32, block: Block): void {
         // possibly run indexOutpoints beforehand to get the outpoint table
-        let runesBlock = changetype<RunesBlock>(block);
         for(let i = 0; i < block.transactions.length; i++) {
             let tx = block.transactions[i];
             // process the current transaction to get the runes balances for each output
@@ -16,32 +17,19 @@ export class RunesPayment extends RunestoneMessage {
             for(let j = 0; j < tx.ins.length; j++) {
                 let input = tx.ins[j];
                 let prev = input.previousOutput();
-                OUTPOINT_TO_RUNES.select(prev.toArrayBuffer());
-                
+                let ptr = OUTPOINT_TO_RUNES.select(prev.toArrayBuffer());
+                let balance = BalanceSheet.load(ptr);
+                for (let k = 0; k < balance.runes.length; k++) {
+                    let rune = balance.runes[k];
+                    // format for rune payment is /runespayments/runeid/height/recipient/sender/amt
+                    const runePtr = RUNES_PAYMENTS_TABLE.select(rune).selectValue<u32>(height).keyword("/");
+                }
                 // need to check is the edict specifies who recipient is meant to be
 
 
-                // let input = tx.inputs[j];
-                // let outpoint = runesBlock.outpoints.get(input.outpoint);
-                // if(outpoint) {
-                //     // if the outpoint exists, then we can check the runes balance sheet to see if the 
-                //     // rune exists
-                //     let rune = runesBlock.runes.get(outpoint.rune);
-                //     if(rune) {
-                //         // if the rune exists, then we can add the payment to the rune
-                //         rune.payments.push({
-                //             height: height,
-                //             txid: tx.txid,
-                //             index: j,
-                //             amount: outpoint.amount
-                //         });
-                //     }
-                // }
+                // the rune edict will specify the output on the transaction where it is being sent
             }
         }
     }
 
-    processBlock(height: u32, block: Block): void {
-        
-    }
 }
--- assembly/tables/tables.ts
+++ assembly/tables/tables.ts
@@ -1,5 +1,5 @@
 import { IndexPointer } from "metashrew-as/assembly/indexer/tables";
 
 export const SENDER_TO_RECIPIENT = IndexPointer.for("/senders/to/recipients");
-export const RUNES_PAYMENTS_TABLE = IndexPointer.for("/payments/");
+export const RUNES_PAYMENTS_TABLE = IndexPointer.for("/runespayments/");
 export const 
